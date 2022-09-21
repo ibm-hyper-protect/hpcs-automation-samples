26db7d97e3effdd4a3a7116ae60463795fe7e69d
vishwabmc <vishwanath@in.ibm.com>
2022-09-21 13:40:06 +0530

    Re-arrange variables and check in images files
    Signed-off-by: vishwabmc <vishwanath@in.ibm.com>

--- uko/aws_keystore/variables.tf
+++ uko/aws_keystore/variables.tf
@@ -1,31 +1,18 @@
-variable "hpcs_uko_vault_name" {
-  type        = string
-  default     = "hpcs-uko-demo-vault-terraform"
-  description = "This is the vault inside UKO that will contain KeyStores and keys"
-}
-
-variable "hpcs_uko_instance_name" {
-  type        = string
-  default     = "hpcs-uko-demo-instance"
-  description = "Name of the HPCS UKO instance created"
-}
-
-variable "aws_region" {
-  type        = string
-  default     = "us-east-2"
-  description = "AWS region onto which the Keystore needs to be deployed"
-}
-
 variable "ibm_cloud_region" {
   type        = string
   default     = "us-east"
   description = "IBM region onto which the UKO instance needs to deployed"
 }
 
-variable "aws_keystore_name" {
+variable "ibm_cloud_api_key" {
   type        = string
-  default     = "uko-aws-demo-keystore"
-  description = "Name of the Keystore that is to be created in AWS"
+  description = "IAM API key that is created on IBM cloud console"
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-2"
+  description = "AWS region onto which the Keystore needs to be deployed"
 }
 
 variable "aws_iam_access_key_id" {
@@ -38,16 +25,34 @@ variable "aws_iam_secret_access_key" {
   description = "AWS KMS Secret Access Key used for login"
 }
 
-variable "aws_kms_key_label" {
+variable "hpcs_uko_instance_name" {
   type        = string
-  default     = "uko-aws-demo-aes-key"
-  description = "Name of the key alias for AES key pushed to AWS"
+  default     = "hpcs-uko-demo-instance-east"
+  description = "Name of the HPCS UKO instance created"
+}
+
+variable "hpcs_uko_vault_name" {
+  type        = string
+  default     = "hpcs-uko-demo-vault-terraform"
+  description = "This is the vault inside UKO that will contain KeyStores and keys"
+}
+
+variable "aws_keystore_name" {
+  type        = string
+  default     = "uko-aws-demo-keystore"
+  description = "Name of the Keystore that is to be created in AWS"
 }
 
 variable "uko_key_template_name" {
   type        = string
   default     = "uko-aws-demo-key-template"
   description = "Name to be given for the created key template"
+}
+
+variable "aws_kms_key_label" {
+  type        = string
+  default     = "uko-aws-demo-aes-key"
+  description = "Name of the key alias for AES key pushed to AWS"
 }
 
 variable "aws_s3_bucket_name" {
